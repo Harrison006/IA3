a3abce679aa01dc1601f4441b17b6a75f322c5b3
Harrison Bostock <harrison@bostockgroup.com>
2023-07-26 04:12:50 +0000

    Commit Script

--- test1.py
+++ test1.py
@@ -2,11 +2,11 @@ import requests
 
 first_name = 'test'
 last_name = 'bloke'
-email = 'test@gmail.com'
-gender = 'male'
-address = '123 m st'
+#email = 'test@gmail.com'
+#gender = 'male'
+address = '123st'
 suburb = 'place'
-phone = '123456789'
+#phone = '123456789'
 
 
 base_url = "https://api.infrasolutions.au/api/add_patient?firstname={}&lastname={}&email={}&gender={}&address{}&suburb{}&phone={}"
