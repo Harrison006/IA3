8a9ad857be8a6b3de5854eba6ffc9838c751f31c
Harrison Bostock <harrison@bostockgroup.com>
2023-08-02 03:48:59 +0000

    Commit Script

--- api.py
+++ api.py
@@ -5,30 +5,36 @@ import requests
 
 class Datastore:
     def get_usernames(self):
+        # url for Api Calls
         response = requests.get("https://api.infrasolutions.au/api/get_usernames")
         json_respnose = response.json()
         return json_respnose
 
     def get_staff_names(self):
+        # url for Api Calls
         response = requests.get("https://api.infrasolutions.au/api/get_staff_names")
         json_respnose = response.json()
         return json_respnose
 
     def get_assessments(self, firstname, lastname):
+        # url for Api Calls
         response = requests.get(
-            "https://api.infrasolutions.au/api/get_assessments?firstname={}&lastname={}"
-        )
+            f"https://api.infrasolutions.au/api/get_assessments?firstname={firstname}&lastname={lastname}"
+        )    # Using an F string to demonstrate different types of ways of inserting varibles into code
+        
         json_respnose = response.json()
         return json_respnose
 
     def add_patient(self, first_name, last_name, email, gender, address, suburb, phone):
+        # Initial URL for the API
         base_url = "https://api.infrasolutions.au/api/add_patient?first_name={}&last_name={}&email={}&gender={}&address={}&suburb={}&phone={}"
         url = base_url.format(
             first_name, last_name, email, gender, address, suburb, phone
-        )
+        ) # Formating the URL afterwards with the correct varibles 
         response = requests.post(url)
+        # Calling the API
         return response
-
+        # Returning the response
     def get_password(self, username):
         # url for Api Calls
         base_url = "https://api.infrasolutions.au/api/get_password?username={}"
